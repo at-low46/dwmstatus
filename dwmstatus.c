8ff63294736fa3310c455ecc090401f895090ac9
at-low46 <105537348+at-low46@users.noreply.github.com>
2022-07-12 06:43:14 -0300

    Change formatting and displayed data

--- dwmstatus.c
+++ dwmstatus.c
@@ -17,9 +17,8 @@
 
 #include <X11/Xlib.h>
 
-char *tzargentina = "America/Buenos_Aires";
 char *tzutc = "UTC";
-char *tzberlin = "Europe/Berlin";
+char *tzsaopaulo = "America/Sao_Paulo";
 
 static Display *dpy;
 
@@ -181,11 +180,8 @@ main(void)
 	char *status;
 	char *avgs;
 	char *bat;
-	char *bat1;
-	char *tmar;
 	char *tmutc;
-	char *tmbln;
-	char *t0, *t1, *t2;
+	char *tmsp;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
@@ -195,28 +191,18 @@ main(void)
 	for (;;sleep(60)) {
 		avgs = loadavg();
 		bat = getbattery("/sys/class/power_supply/BAT0");
-		bat1 = getbattery("/sys/class/power_supply/BAT1");
-		tmar = mktimes("%H:%M", tzargentina);
 		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
-		t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
-		t1 = gettemperature("/sys/devices/virtual/hwmon/hwmon2", "temp1_input");
-		t2 = gettemperature("/sys/devices/virtual/hwmon/hwmon4", "temp1_input");
+		tmsp = mktimes("%a %Y-%m-%d %H:%M %Z", tzsaopaulo);
 
-		status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s",
-				t0, t1, t2, avgs, bat, bat1, tmar, tmutc,
-				tmbln);
+		status = smprintf(" L:%s | B:%s | U:%s | %s ",
+				avgs, bat, tmutc,
+				tmsp);
 		setstatus(status);
 
-		free(t0);
-		free(t1);
-		free(t2);
 		free(avgs);
 		free(bat);
-		free(bat1);
-		free(tmar);
 		free(tmutc);
-		free(tmbln);
+		free(tmsp);
 		free(status);
 	}
 
